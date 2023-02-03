c03ff9f401b34ba48df45f8390658d018ea2a74d
David Vilela <dvilelaf@gmail.com>
2023-02-03 20:09:39 +0100

    feat: impact evaluator

---
+++ packages/valory/skills/impact_evaluator_abci/composition.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2022-2023 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This package contains round behaviours of DynamicNFTAbciApp."""
+import packages.valory.skills.dynamic_nft_abci.rounds as DynamicNFTAbci
+import packages.valory.skills.registration_abci.rounds as RegistrationAbci
+import packages.valory.skills.reset_pause_abci.rounds as ResetAndPauseAbci
+import packages.valory.skills.score_read_abci.rounds as ScoreReadAbciAbci
+import packages.valory.skills.score_write_abci.rounds as ScoreWriteAbciAbci
+from packages.valory.skills.abstract_round_abci.abci_app_chain import (
+    AbciAppTransitionMapping,
+    chain,
+)
+
+
+# Here we define how the transition between the FSMs should happen
+# more information here: https://docs.autonolas.network/fsm_app_introduction/#composition-of-fsm-apps
+abci_app_transition_mapping: AbciAppTransitionMapping = {
+    RegistrationAbci.FinishedRegistrationRound: ScoreReadAbciAbci.TwitterObservationRound,
+    ScoreReadAbciAbci.FinishedScoringRound: ScoreWriteAbciAbci.RandomnessRound,
+    ScoreWriteAbciAbci.FinishedVerificationRound: DynamicNFTAbci.NewTokensRound,
+    DynamicNFTAbci.FinishedDBUpdateRound: ResetAndPauseAbci.ResetAndPauseRound,
+    ResetAndPauseAbci.FinishedResetAndPauseRound: DynamicNFTAbci.NewTokensRound,
+    ResetAndPauseAbci.FinishedResetAndPauseErrorRound: RegistrationAbci.RegistrationRound,
+}
+
+ImpactEvaluatorSkillAbciApp = chain(
+    (
+        RegistrationAbci.AgentRegistrationAbciApp,
+        ScoreReadAbciAbci.DynamicNFTAbciApp,
+        ScoreWriteAbciAbci.DynamicNFTAbciApp,
+        DynamicNFTAbci.DynamicNFTAbciApp,
+        ResetAndPauseAbci.ResetPauseAbciApp,
+    ),
+    abci_app_transition_mapping,
+)
---
+++ packages/valory/skills/impact_evaluator_abci/models.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2022-2023 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the shared state for the abci skill of ImpactEvaluatorSkillAbciApp."""
+
+from typing import Any
+
+from packages.valory.skills.abstract_round_abci.models import (
+    BenchmarkTool as BaseBenchmarkTool,
+)
+from packages.valory.skills.abstract_round_abci.models import Requests as BaseRequests
+from packages.valory.skills.abstract_round_abci.models import (
+    SharedState as BaseSharedState,
+)
+from packages.valory.skills.dynamic_nft_abci.models import (
+    Params as DynamicNFTAbciParams,
+)
+from packages.valory.skills.dynamic_nft_abci.rounds import Event as DynamicNFTEvent
+from packages.valory.skills.impact_evaluator_abci.composition import (
+    ImpactEvaluatorSkillAbciApp,
+)
+from packages.valory.skills.reset_pause_abci.rounds import Event as ResetPauseEvent
+from packages.valory.skills.score_read_abci.models import Params as ScoreReadAbciParams
+from packages.valory.skills.score_read_abci.rounds import Event as ScoreReadEvent
+from packages.valory.skills.score_write_abci.models import (
+    Params as ScoreWriteAbciParams,
+)
+from packages.valory.skills.score_write_abci.rounds import Event as ScoreWriteEvent
+
+
+ScoreReadParams = ScoreReadAbciParams
+ScoreWriteParams = ScoreWriteAbciParams
+DynamicNFTParams = DynamicNFTAbciParams
+Requests = BaseRequests
+BenchmarkTool = BaseBenchmarkTool
+
+MARGIN = 5
+
+
+class SharedState(BaseSharedState):
+    """Keep the current shared state of the skill."""
+
+    abci_app_cls = ImpactEvaluatorSkillAbciApp
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize the state."""
+        super().__init__(*args, abci_app_cls=ImpactEvaluatorSkillAbciApp, **kwargs)
+
+    def setup(self) -> None:
+        """Set up."""
+        super().setup()
+        ImpactEvaluatorSkillAbciApp.event_to_timeout[
+            ScoreReadEvent.ROUND_TIMEOUT
+        ] = self.context.params.round_timeout_seconds
+        ImpactEvaluatorSkillAbciApp.event_to_timeout[
+            ScoreWriteEvent.ROUND_TIMEOUT
+        ] = self.context.params.round_timeout_seconds
+        ImpactEvaluatorSkillAbciApp.event_to_timeout[
+            DynamicNFTEvent.ROUND_TIMEOUT
+        ] = self.context.params.round_timeout_seconds
+        ImpactEvaluatorSkillAbciApp.event_to_timeout[
+            ResetPauseEvent.ROUND_TIMEOUT
+        ] = self.context.params.round_timeout_seconds
+        ImpactEvaluatorSkillAbciApp.event_to_timeout[
+            ResetPauseEvent.RESET_AND_PAUSE_TIMEOUT
+        ] = (self.context.params.observation_interval + MARGIN)
+
+
+class Params(DynamicNFTParams):
+    """A model to represent params for multiple abci apps."""
