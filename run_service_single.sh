f91f017c41d644b87699fb06619d5bdb3b568838
David Vilela <dvilelaf@gmail.com>
2023-09-26 16:44:30 +0200

    feat: single agent service

---
+++ run_service_single.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Go into the relevant venv
+#cd /home/david/Valory/repos/IEKit && pipenv shell
+
+# Set env vars
+export $(grep -v '^#' .1env | xargs)
+
+# Push packages and fetch service
+make clean
+
+autonomy push-all
+
+autonomy fetch --local --service valory/impact_evaluator_local && cd impact_evaluator_local
+
+# Build the image
+autonomy build-image
+
+# Copy the keys and build the deployment
+cp $KEY_DIR/keys1.json ./keys.json
+autonomy deploy build -ltm
+
+# Run the deployment
+autonomy deploy run --build-dir abci_build/
