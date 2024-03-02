22a3cc95ed91cd1397c094df8b876e6c6c24b5f6
David Vilela <dvilelaf@gmail.com>
2024-03-02 16:15:33 -0700

    feat: farcaster skill

---
+++ packages/valory/skills/farcaster_write_abci/behaviours.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2024 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This package contains round behaviours of FarcasterWriteAbciApp."""
+
+import json
+from abc import ABC
+from typing import Generator, Optional, Set, Type, cast
+
+from packages.valory.connections.farcaster.connection import (
+    PUBLIC_ID as FARCASTER_CONNECTION_PUBLIC_ID,
+)
+from packages.valory.protocols.srr.message import SrrMessage
+from packages.valory.skills.abstract_round_abci.base import AbstractRound
+from packages.valory.skills.abstract_round_abci.behaviours import (
+    AbstractRoundBehaviour,
+    BaseBehaviour,
+)
+from packages.valory.skills.abstract_round_abci.common import (
+    RandomnessBehaviour,
+    SelectKeeperBehaviour,
+)
+from packages.valory.skills.abstract_round_abci.models import Requests
+from packages.valory.skills.farcaster_write_abci.dialogues import (
+    FarcasterDialogue,
+    FarcasterDialogues,
+)
+from packages.valory.skills.farcaster_write_abci.models import Params
+from packages.valory.skills.farcaster_write_abci.payloads import FarcasterWritePayload
+from packages.valory.skills.farcaster_write_abci.rounds import (
+    FarcasterWriteAbciApp,
+    FarcasterWriteRound,
+    RandomnessFarcasterRound,
+    RandomnessPayload,
+    SelectKeeperFarcasterRound,
+    SelectKeeperPayload,
+    SynchronizedData,
+)
+
+
+class BaseFarcasterWriteBehaviour(BaseBehaviour, ABC):
+    """Base behaviour for the common apps' skill."""
+
+    @property
+    def synchronized_data(self) -> SynchronizedData:
+        """Return the synchronized data."""
+        return cast(SynchronizedData, super().synchronized_data)
+
+    @property
+    def params(self) -> Params:
+        """Return the params."""
+        return cast(Params, super().params)
+
+
+class RandomnessFarcasterWriteBehaviour(RandomnessBehaviour, BaseFarcasterWriteBehaviour):
+    """Retrieve randomness."""
+
+    matching_round = RandomnessFarcasterRound
+    payload_class = RandomnessPayload
+
+
+class SelectKeeperFarcasterWriteBehaviour(
+    SelectKeeperBehaviour, BaseFarcasterWriteBehaviour
+):
+    """Select the keeper agent."""
+
+    matching_round = SelectKeeperFarcasterRound
+    payload_class = SelectKeeperPayload
+
+
+class FarcasterWriteBehaviour(BaseFarcasterWriteBehaviour):
+    """FarcasterWriteBehaviour"""
+
+    matching_round: Type[AbstractRound] = FarcasterWriteRound
+
+    def _i_am_casting(self) -> bool:
+        """Indicates if the current agent is casting or not."""
+        return (
+            self.context.agent_address
+            == self.synchronized_data.most_voted_keeper_address
+        )
+
+    def async_act(self) -> Generator[None, None, None]:
+        """Do the action"""
+        if self._i_am_casting():
+            yield from self._cast()
+        else:
+            yield from self._wait_for_cast()
+
+    def _wait_for_cast(self) -> Generator:
+        """Do the non-sender action."""
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            self.context.logger.info(
+                f"Waiting for the keeper to do its keeping: {self.synchronized_data.most_voted_keeper_address}"
+            )
+            yield from self.wait_until_round_end()
+        self.set_done()
+
+    def _cast(self) -> Generator:
+        """Do the sender action"""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            write_index = self.synchronized_data.write_index
+            data = self.synchronized_data.write_data[write_index]
+            text = data["text"]
+            self.context.logger.info(f"Creating cast with text: {text}")
+            response = yield from self._create_cast(text=text)
+            if response.error:
+                self.context.logger.error(
+                    f"Writing cast failed with following error message: {response.payload}"
+                )
+                payload_data = {"success": False, "cast_id": None}
+            else:
+                self.context.logger.info(f"Posted cast with ID: {response.payload['cast_id']}")
+                payload_data = {"success": True, "cast_id": response.payload["cast_id"]}
+
+            payload = FarcasterWritePayload(
+                sender=self.context.agent_address,
+                content=json.dumps(
+                    payload_data,
+                    sort_keys=True,
+                ),
+            )
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+    def _create_cast(
+        self,
+        text: str,
+        credentials: dict,
+    ) -> Generator[None, None, SrrMessage]:
+        """Send an http request message from the skill context."""
+        farcaster_dialogues = cast(FarcasterDialogues, self.context.farcaster_dialogues)
+        farcaster_message, farcaster_dialogue = farcaster_dialogues.create(
+            counterparty=str(FARCASTER_CONNECTION_PUBLIC_ID),
+            performative=SrrMessage.Performative.REQUEST,
+            payload=json.dumps(
+                {"method": "post_cast", "args": {"text": text}}
+            ),
+        )
+        farcaster_message = cast(SrrMessage, farcaster_message)
+        farcaster_dialogue = cast(FarcasterDialogue, farcaster_dialogue)
+        response = yield from self._do_farcaster_request(
+            farcaster_message, farcaster_dialogue
+        )
+        return response
+
+    def _do_farcaster_request(
+        self,
+        message: SrrMessage,
+        dialogue: FarcasterDialogue,
+        timeout: Optional[float] = None,
+    ) -> Generator[None, None, SrrMessage]:
+        """Do a request and wait the response, asynchronously."""
+
+        self.context.outbox.put_message(message=message)
+        request_nonce = self._get_request_nonce_from_dialogue(dialogue)
+        cast(Requests, self.context.requests).request_id_to_callback[
+            request_nonce
+        ] = self.get_callback_request()
+        response = yield from self.wait_for_message(timeout=timeout)
+        return response
+
+
+class FarcasterWriteRoundBehaviour(AbstractRoundBehaviour):
+    """FarcasterWriteRoundBehaviour"""
+
+    initial_behaviour_cls = RandomnessFarcasterWriteBehaviour
+    abci_app_cls = FarcasterWriteAbciApp  # type: ignore
+    behaviours: Set[Type[BaseBehaviour]] = [
+        RandomnessFarcasterWriteBehaviour,
+        SelectKeeperFarcasterWriteBehaviour,
+        FarcasterWriteBehaviour,
+    ]
---
+++ packages/valory/skills/farcaster_write_abci/rounds.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2024 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This package contains the rounds of FarcasterWriteAbciApp."""
+
+import json
+from enum import Enum
+from typing import Dict, FrozenSet, List, Optional, Set, Tuple, cast
+
+from packages.valory.skills.abstract_round_abci.base import (
+    AbciApp,
+    AbciAppTransitionFunction,
+    AppState,
+    BaseSynchronizedData,
+    CollectSameUntilThresholdRound,
+    DegenerateRound,
+    EventToTimeout,
+    OnlyKeeperSendsRound,
+    get_name,
+)
+from packages.valory.skills.farcaster_write_abci.payloads import (
+    FarcasterWritePayload,
+    RandomnessPayload,
+    SelectKeeperPayload,
+)
+
+
+class Event(Enum):
+    """FarcasterWriteAbciApp Events"""
+
+    DONE = "done"
+    CONTINUE = "continue"
+    NO_MAJORITY = "no_majority"
+    ROUND_TIMEOUT = "round_timeout"
+    API_ERROR = "api_error"
+
+
+class SynchronizedData(BaseSynchronizedData):
+    """
+    Class to represent the synchronized data.
+
+    This data is replicated by the tendermint application.
+    """
+
+    @property
+    def write_data(self) -> list:
+        """Get the write_stream_id."""
+        return cast(list, self.db.get_strict("write_data"))
+
+    @property
+    def write_index(self) -> int:
+        """Get the write_index."""
+        return cast(int, self.db.get("write_index", 0))
+
+    @property
+    def cast_ids(self) -> List[int]:
+        """List of posted cast ids."""
+        return cast(list, self.db.get("cast_ids", []))
+
+
+class RandomnessFarcasterRound(CollectSameUntilThresholdRound):
+    """A round for generating randomness"""
+
+    payload_class = RandomnessPayload
+    synchronized_data_class = SynchronizedData
+    done_event = Event.DONE
+    no_majority_event = Event.NO_MAJORITY
+    collection_key = get_name(SynchronizedData.participant_to_randomness)
+    selection_key = (
+        get_name(SynchronizedData.most_voted_randomness),
+        get_name(SynchronizedData.most_voted_randomness),
+    )
+
+
+class SelectKeeperFarcasterRound(CollectSameUntilThresholdRound):
+    """A round in which a keeper is selected for transaction submission"""
+
+    payload_class = SelectKeeperPayload
+    synchronized_data_class = SynchronizedData
+    done_event = Event.DONE
+    no_majority_event = Event.NO_MAJORITY
+    collection_key = get_name(SynchronizedData.participant_to_selection)
+    selection_key = get_name(SynchronizedData.most_voted_keeper_address)
+
+
+class FarcasterWriteRound(OnlyKeeperSendsRound):
+    """FarcasterWriteRound"""
+
+    payload_class = FarcasterWritePayload
+    synchronized_data_class = SynchronizedData
+
+    def end_block(
+        self,
+    ) -> Optional[
+        Tuple[BaseSynchronizedData, Enum]
+    ]:  # pylint: disable=too-many-return-statements
+        """Process the end of the block."""
+        if self.keeper_payload is None:
+            return None
+        keeper_payload = json.loads(
+            cast(FarcasterWritePayload, self.keeper_payload).content
+        )
+        if not keeper_payload["success"]:
+            return self.synchronized_data, Event.API_ERROR
+
+        synchronized_data = cast(SynchronizedData, self.synchronized_data)
+        write_index = synchronized_data.write_index + 1
+        cast_ids = synchronized_data.cast_ids
+
+        cast_ids.append(keeper_payload["cast_id"])
+
+        finished_casting = write_index == len(synchronized_data.write_data)
+
+        return (
+            self.synchronized_data.update(
+                synchronized_data_class=SynchronizedData,
+                **{
+                    get_name(SynchronizedData.cast_ids): cast_ids,
+                    get_name(SynchronizedData.write_index): 0
+                    if finished_casting
+                    else write_index,
+                }
+            ),
+            Event.DONE if finished_casting else Event.CONTINUE,
+        )
+
+
+class FinishedFarcasterWriteRound(DegenerateRound):
+    """FinishedFarcasterWriteRound"""
+
+
+class FarcasterWriteAbciApp(AbciApp[Event]):
+    """FarcasterWriteAbciApp"""
+
+    initial_round_cls: AppState = RandomnessFarcasterRound
+    initial_states: Set[AppState] = {RandomnessFarcasterRound}
+    transition_function: AbciAppTransitionFunction = {
+        RandomnessFarcasterRound: {
+            Event.DONE: SelectKeeperFarcasterRound,
+            Event.NO_MAJORITY: RandomnessFarcasterRound,
+            Event.ROUND_TIMEOUT: RandomnessFarcasterRound,
+        },
+        SelectKeeperFarcasterRound: {
+            Event.DONE: FarcasterWriteRound,
+            Event.NO_MAJORITY: RandomnessFarcasterRound,
+            Event.ROUND_TIMEOUT: RandomnessFarcasterRound,
+        },
+        FarcasterWriteRound: {
+            Event.DONE: FinishedFarcasterWriteRound,
+            Event.CONTINUE: FarcasterWriteRound,
+            Event.API_ERROR: RandomnessFarcasterRound,
+            Event.ROUND_TIMEOUT: RandomnessFarcasterRound,
+        },
+        FinishedFarcasterWriteRound: {},
+    }
+    final_states: Set[AppState] = {FinishedFarcasterWriteRound}
+    event_to_timeout: EventToTimeout = {
+        Event.ROUND_TIMEOUT: 30.0,
+    }
+    cross_period_persisted_keys: FrozenSet[str] = frozenset()
+    db_pre_conditions: Dict[AppState, Set[str]] = {
+        RandomnessFarcasterRound: set(),
+    }
+    db_post_conditions: Dict[AppState, Set[str]] = {
+        FinishedFarcasterWriteRound: set(),
+    }
