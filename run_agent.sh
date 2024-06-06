d1c4ec14c6755ba63c4fd042341c9a25debce5c2
David Vilela <dvilelaf@gmail.com>
2024-06-06 17:38:23 +0200

    fix: run_agent script

--- run_agent.sh
+++ run_agent.sh
@@ -1,13 +1,13 @@
-rm -r impact_evaluator
+if test -d impact_evaluator; then
+  echo "Removing previous agent build"
+  rm -r impact_evaluator
+fi
+
 find . -empty -type d -delete  # remove empty directories to avoid wrong hashes
 autonomy packages lock
-autonomy fetch --local --agent valory/impact_evaluator && cd impact_evaluator
-
-# Replace aea-config with the configured one
-aeaconfig="/home/david/Cloud/env/contribute/aea-config.yaml"
-if [ -e "$aeaconfig" ]; then
-  cp $aeaconfig .
-fi
+autonomy fetch --local --agent valory/impact_evaluator
+python scripts/aea-config-replace.py
+cd impact_evaluator
 
 cp $PWD/../ethereum_private_key.txt .
 autonomy add-key ethereum ethereum_private_key.txt
