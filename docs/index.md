f6287bfd628c638b8b96fd28538c89813e75d494
Anna Sambrook <annasambrook@gmail.com>
2024-08-09 16:49:14 +0100

    tests: add tests for mech interact states/base and models

--- docs/index.md
+++ docs/index.md
@@ -31,7 +31,7 @@ In order to run a local demo service based on the IEKit:
 2. Fetch the IEKit.
 
     ```bash
-    autonomy fetch valory/impact_evaluator:0.1.0:bafybeidrdmkkugdr4qhrrpciivmvyo4cyf6hjqfeseogn45jfbkoeqkgsu --service
+    autonomy fetch valory/impact_evaluator:0.1.0:bafybeiefd3ty2axcezdi7o2eweacyop5uyefllxedmlfr7dv24k2uavvwm --service
     ```
 
 3. Build the Docker image of the service agents
--- packages/valory/skills/mech_interact_abci/tests/test_models.py
+++ packages/valory/skills/mech_interact_abci/tests/test_models.py
@@ -18,9 +18,37 @@
 # ------------------------------------------------------------------------------
 
 """Test the models.py module of the MechInteract."""
+import pytest
+from hexbytes import HexBytes
 
 from packages.valory.skills.abstract_round_abci.test_tools.base import DummyContext
-from packages.valory.skills.mech_interact_abci.models import SharedState
+from packages.valory.skills.abstract_round_abci.tests.test_models import (
+    BASE_DUMMY_PARAMS,
+    BASE_DUMMY_SPECS_CONFIG,
+)
+from packages.valory.skills.mech_interact_abci.models import (
+    MechParams,
+    MechResponseSpecs,
+    MultisendBatch,
+    SharedState,
+)
+
+
+class TestMechResponseSpecs:
+    """Test MechResponseSpecs of MechInteract models."""
+
+    def test_initialization(self) -> None:
+        """Test initialization."""
+        MechResponseSpecs(
+            **BASE_DUMMY_SPECS_CONFIG,
+            response_key="value",
+            response_index=0,
+            response_type="float",
+            error_key="error",
+            error_index=None,
+            error_type="str",
+            error_data="error text",
+        )
 
 
 class TestSharedState:
@@ -29,3 +57,44 @@ class TestSharedState:
     def test_initialization(self) -> None:
         """Test initialization."""
         SharedState(name="", skill_context=DummyContext())
+
+
+class TestMechParams:
+    """Test MechParams of MechInteract models."""
+
+    def setup(self) -> None:
+        """Set up the tests."""
+        self.mech_params = MechParams(
+            **BASE_DUMMY_PARAMS,
+            multisend_address="dummy_multisend_address",
+            multisend_batch_size=1,
+            mech_contract_address="dummy_mech_contract_address",
+            ipfs_address="dummy_ipfs_address",
+            mech_interaction_sleep_time=1,
+        )
+
+    def test_initialization(self) -> None:
+        """Test initialization"""
+        assert isinstance(self.mech_params, MechParams)
+
+    @pytest.mark.parametrize(
+        "dummy_ipfs_address", ["dummy_ipfs_address", "dummy_ipfs_address/"]
+    )
+    def test_ipfs_address(self, dummy_ipfs_address) -> None:
+        """Test ipfs address."""
+        print(self.mech_params.ipfs_address)
+        self.mech_params.__dict__["_frozen"] = False
+        self.mech_params._ipfs_address = dummy_ipfs_address
+        ipfs_address = self.mech_params.ipfs_address
+        assert ipfs_address == "dummy_ipfs_address/"
+
+
+class TestMultisendBatch:
+    """Test MultisendBatch of MechInteract models."""
+
+    def test_initialization(self) -> None:
+        """Test initialization."""
+        MultisendBatch(
+            to="",
+            data=HexBytes("0xabcdef1234567890"),
+        )
