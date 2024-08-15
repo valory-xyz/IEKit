080dc773a74cb6c4e2a8c7b05017e95b5721dcbc
Anna Sambrook <annasambrook@gmail.com>
2024-08-15 17:15:50 +0100

    added tests for mech_interact request behaviour

--- packages/valory/skills/mech_interact_abci/tests/test_behaviours.py
+++ packages/valory/skills/mech_interact_abci/tests/test_behaviours.py
@@ -21,15 +21,22 @@
 
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Any, Dict, Hashable, Optional, Type
+from typing import Any, Dict, Hashable, Optional, Type, cast
+
+from unittest import mock
+from unittest.mock import MagicMock
 
 import pytest
 
+from packages.valory.protocols.contract_api import ContractApiMessage
+from packages.valory.protocols.ledger_api import LedgerApiMessage
 from packages.valory.skills.abstract_round_abci.base import AbciAppDB
+from packages.valory.skills.abstract_round_abci.behaviour_utils import make_degenerate_behaviour
 from packages.valory.skills.abstract_round_abci.behaviours import BaseBehaviour
 from packages.valory.skills.abstract_round_abci.test_tools.base import (
     FSMBehaviourBaseCase,
 )
+from packages.valory.skills.abstract_round_abci.tests.test_common import dummy_generator
 from packages.valory.skills.mech_interact_abci.behaviours.base import (
     MechInteractBaseBehaviour,
 )
@@ -42,10 +49,12 @@ from packages.valory.skills.mech_interact_abci.behaviours.response import (
 from packages.valory.skills.mech_interact_abci.behaviours.round_behaviour import (
     MechInteractRoundBehaviour,
 )
+from packages.valory.skills.mech_interact_abci.models import SharedState
 from packages.valory.skills.mech_interact_abci.states.base import (
     Event,
-    SynchronizedData,
+    SynchronizedData, MechMetadata,
 )
+from packages.valory.skills.mech_interact_abci.states.final_states import FinishedMechRequestRound
 
 
 @dataclass
@@ -55,6 +64,7 @@ class BehaviourTestCase:
     name: str
     initial_data: Dict[str, Hashable]
     event: Event
+    return_value: Any
     kwargs: Dict[str, Any] = field(default_factory=dict)
 
 
@@ -80,11 +90,14 @@ class BaseMechInteractTest(FSMBehaviourBaseCase):
 
         data = data if data is not None else {}
         self.fast_forward_to_behaviour(
-            self.behaviour,
-            self.behaviour_class.behaviour_id,
+            self.behaviour,  # type: ignore
+            self.behaviour_class.auto_behaviour_id(),
             SynchronizedData(AbciAppDB(setup_data=AbciAppDB.data_to_lists(data))),
         )
-        assert self.current_behaviour_id == self.behaviour_class.behaviour_id
+        assert (
+                self.behaviour.current_behaviour.auto_behaviour_id()  # type: ignore
+                == self.behaviour_class.auto_behaviour_id()
+        )
 
     def complete(self, event: Event) -> None:
         """Complete test"""
@@ -93,25 +106,97 @@ class BaseMechInteractTest(FSMBehaviourBaseCase):
         self.mock_a2a_transaction()
         self._test_done_flag_set()
         self.end_round(done_event=event)
-        assert self.current_behaviour_id == self.next_behaviour_class.behaviour_id
+        assert (
+                self.behaviour.current_behaviour.auto_behaviour_id()  # type: ignore
+                == self.next_behaviour_class.auto_behaviour_id()
+        )
 
 
 class TestMechRequestBehaviour(BaseMechInteractTest):
     """Tests MechRequestBehaviour"""
 
-    # TODO: set next_behaviour_class
     behaviour_class: Type[BaseBehaviour] = MechRequestBehaviour
-    next_behaviour_class: Type[BaseBehaviour] = ...
+    next_behaviour_class: Type[BaseBehaviour] = make_degenerate_behaviour(FinishedMechRequestRound)
 
-    # TODO: provide test cases
-    @pytest.mark.parametrize("test_case", [])
-    def test_run(self, test_case: BehaviourTestCase) -> None:
+    @pytest.mark.parametrize(
+        "test_case, kwargs",
+        [
+            (
+                BehaviourTestCase(
+                    "Happy path - no mech request",
+                    initial_data=dict(mech_requests=[]),
+                    event=Event.DONE,
+                ),
+                {},
+            ),
+            (
+                    BehaviourTestCase(
+                        "Happy path",
+                        initial_data=dict(mech_requests='[{"prompt": "dummy_prompt_1", "tool": "dummy_tool_1", "nonce": "dummy_nonce_1"}, {"prompt": "dummy_prompt_2", "tool": "dummy_tool_2", "nonce": "dummy_nonce_2"}]',
+                                          safe_contract_address='mock_safe_contract_address'),
+                        event=Event.DONE,
+                        return_value={
+                            "ledger": MagicMock(state=MagicMock(body={"get_balance_result": "10"})),
+                            "contract": MagicMock(
+                                performative=ContractApiMessage.Performative.RAW_TRANSACTION,
+                                raw_transaction=MagicMock(body={
+                                    "token": 10 ,
+                                    "wallet": "mock_safe_contract_address",
+                                    "data": "0x0000deadbeef0000",
+                                    "request_data": "dummy_request_data",
+                                })
+                            ),
+                            "metadata_hash": "QmTzQ1iVRmDDeh8aW5HX99o9dsPwzUo9tb5w1DLT8PaJfM",
+                            "multisend_safe_tx_hash": MagicMock(
+                                performative=ContractApiMessage.Performative.STATE,
+                                state=MagicMock(body={
+                                    "tx_hash": "0x0000000000000000000000000000000000000000000000000000000000000000"
+                                })
+                            )
+                        }
+                    ),
+                    {},
+            ),
+        ],
+    )
+    def test_run(self, test_case: BehaviourTestCase, kwargs) -> None:
         """Run tests."""
+        with mock.patch.object(
+                MechRequestBehaviour,
+                "get_ledger_api_response",
+                dummy_generator(test_case.return_value["ledger"]),
+        ), mock.patch.object(
+                MechRequestBehaviour,
+                "get_contract_api_response",
+                dummy_generator(test_case.return_value["contract"]),
+        ), mock.patch.object(
+                MechRequestBehaviour,
+                "send_to_ipfs",
+                dummy_generator(test_case.return_value["metadata_hash"]),
+        ):
 
-        self.fast_forward(test_case.initial_data)
-        # TODO: mock the necessary calls
-        # self.mock_ ...
-        self.complete(test_case.event)
+            params = cast(SharedState, self._skill.skill_context.params)
+            params.__dict__["_frozen"] = False
+            params.mech_request_price = 1
+            params.mech_contract_address="dummy_mech_contract_address"
+            self.fast_forward(test_case.initial_data)
+            self.behaviour.act_wrapper()
+            self.behaviour.act_wrapper()
+            self.behaviour.act_wrapper()
+            self.behaviour.act_wrapper()
+            self.behaviour.act_wrapper()
+            self.behaviour.act_wrapper()
+            self.behaviour.act_wrapper()
+
+            with mock.patch.object(
+                MechRequestBehaviour,
+                "get_contract_api_response",
+                dummy_generator(test_case.return_value["multisend_safe_tx_hash"])
+            ):
+                self.behaviour.act_wrapper()
+                # self.behaviour.act_wrapper()
+                # self.behaviour.act_wrapper()
+                self.complete(test_case.event)
 
 
 class TestMechResponseBehaviour(BaseMechInteractTest):
