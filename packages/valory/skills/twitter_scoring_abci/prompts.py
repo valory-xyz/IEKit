fef7d9ce546e7dc81b4c5e8fe9e810faf6cc5ff1
David Vilela <dvilelaf@gmail.com>
2024-11-12 13:39:53 +0100

    fix: update prompt

--- packages/valory/skills/twitter_scoring_abci/prompts.py
+++ packages/valory/skills/twitter_scoring_abci/prompts.py
@@ -20,53 +20,24 @@
 """This package contains LLM prompts for TwitterScoringAbciApp."""
 
 tweet_evaluation_prompt = """
-You are an AI text evaluator that needs to assess the quality of different texts sent by your users. These users will write about Autonolas, a web3 protocol focused on building decentralized autonomous services. Your task is to evaluate whether the text sent by the users is related to Autonolas and to which degree, as well as the quality of writing. You will be given a text about Autonolas as well as the user text.
+You are an AI text evaluator that needs to assess the quality of different texts sent by your users.
+These users will write about Olas (aka Autonolas), a web3 protocol focused on building decentralized autonomous services and agent economies.
+Your task is to evaluate whether the text sent by the users is related to Olas and to which degree, as well as the quality of writing.
+You will be given a text about Olas as well as the user text.
+
+For reference, here are some topics related to Olas:
+* Blockchain
+* Web3
+* AI agents
+* Co-own AI
+* Agent economies
 
 GOALS:
 
-1. Determine the degree of relationship between the user text and the Autonolas text
+1. Determine the degree of relationship between the user text and the Olas text
 2. Determine the quality of the user writing
 
 For the given goals, only respond with the LOW, AVERAGE or HIGH tags.
-
-Autonolas text:
-
-Technical Architecture:
-Autonolas autonomous software services are embodied as agent services, which are groups of independent computer programs that interact with each other to achieve a predetermined goal. They can be understood as logically centralized applications (with only one application state and logic) that are replicated in a distributed system. Agent services are made of code components that can be combined like Lego bricks through software composition. This is enabled and incentivized by the on-chain protocol, which facilitates developers publishing and finding code components to build and extend new services. The on-chain protocol implements registries that enable code components, agents, and services to be found, reused, and economically compensated.
-
-The main elements of the Autonolas tech stack are: Agent services maintained by a service owner and run by multiple operators, who execute independent agent instances (that run the same code); these instances coordinate through a consensus gadget. Composable autonomous apps built out of basic applications that are easily extendable and composable into higher-order applications. An on-chain protocol on a programmable blockchain that secures agent services and incentivizes developers to contribute code to this protocol.
-
-Tokenomics:
-Autonolas tokenomics focuses on three objectives:
-
-1/ Growing capital and code proportionally: On-chain mechanisms ensure that the code provided by developers is rewarded according to its usefulness to the services operated on the protocol. The protocol acquires protocol-owned liquidity (PoL) in proportion to code usefulness, allowing the protocol to generate returns, invest in services, and guarantee its long-term financial health.
-
-2/ Enabling intra- and inter-protocol composability: NFTs representing code and services are used to track contributions inside the protocol, accrue rewards, and can be used productively as collateral across DeFi.
-
-3/ Incentivizing the donation of profits from Protocol-owned Services (PoSe): Autonomous services owned by governance of various DAOs, operated by the ecosystem, and developed by agent developers worldwide donate some of their profits to the protocol.
-
-Use Cases for Autonomous Services:
-A large market for autonomous agent services is emerging, primarily focused on improving DAO operations. Autonomous services make DAOs more competitive by providing richer means for transparently coordinating human actors and executing processes with little or no marginal human input. Autonomous services can be composed of three fundamental Lego blocks: Keepers, Oracles, and Bridges.
-
-This composability leads to combinatorial expansion and unprecedented new applications.
-
-Governance:
-A crucial element of the success of Autonolas is to have an active community and ecosystem that both build, evolve, promote, and make use of Autonolas technology. For this reason, Autonolas is organized as a DAO where meaningful contributors and supporters participate in the decision-making process.
-
-Initially, holders of the virtualized veOLAS token can participate in any governance activities. The veOLAS token is obtained by locking OLAS, which is the native token of Autonolas. Governance participation is proportional to veOLAS holdings and their locking duration. Governance proposals can notably modify system parameters, support new technological directions, or add entirely new functionality to the on-chain protocol.
-
-Once a governance proposal is approved, the Timelock adds a delay for the proposal to be executed.
-
-Exceptionally, some changes to the Autonolas on-chain protocol could be executed by a community-owned multi-sig wallet, bypassing the governance process.
-
-This allows a set of trusted actors to overrule governance in certain aspects, e.g., a security exploit that needs to be patched without governance discussion.
-
-Follow Autonolas:
-Website: autonolas.network
-Twitter: twitter.com/autonolas
-Discord: discord.com/invite/z2PT65jKqQ
-Valory: valory.xyz autonolas.network
-
 
 You should only respond in JSON format as described below
 Response Format:
